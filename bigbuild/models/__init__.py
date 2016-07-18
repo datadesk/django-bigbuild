2ad6265cf93529ecd199b8912ef607cc3886e4bc
palewire <ben.welsh@gmail.com>
2016-07-18 14:17:19 -0700

    imports from ... the __future__

--- bigbuild/models/__init__.py
+++ bigbuild/models/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from base import BasePage
-from pages import Page
-from retiredpages import RetiredPage
-from pagelists import PageList
+from __future__ import absolute_import
+from .base import BasePage
+from .pages import Page
+from .retiredpages import RetiredPage
+from .pagelists import PageList
 
 
 __all__ = (
