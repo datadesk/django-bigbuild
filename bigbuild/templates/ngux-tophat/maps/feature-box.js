9dc5e0bd2d212f2c57eceef81eb3964b7e70c3eb
palewire <ben.welsh@gmail.com>
2016-07-25 15:00:50 -0700

    Cut out all of our latimes specific templates stuff to make this app generic

--- bigbuild/templates/ngux-tophat/maps/feature-box.js
+++
@@ -1,7 +0,0 @@
-L.LATFeatureBox = L.LATCornerOverlay.extend({
-    onAdd: function(map) {
-        var container = L.DomUtil.create('div', 'overlay featurebox', L.DomUtil.get('map'));
-        container.innerHTML = this._html;
-        return container;
-    },
-});
