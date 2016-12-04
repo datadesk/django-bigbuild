00410f4539d1222205acd441867040ed93e5de4f
palewire <ben.welsh@gmail.com>
2016-12-04 14:22:07 -0800

    Simplified context_processor to remove our @latimes specific stuff. Fixes #22.

--- bigbuild/context_processors.py
+++ bigbuild/context_processors.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.conf import settings
+from bigbuild import get_base_url
 from bigbuild import get_repo_branch
 
 
-def environment(request):
+def bigbuild(request):
     """
     Adds env-related context variables to the context.
     """
     return {
-        'DEVELOPMENT': settings.DEVELOPMENT,
-        'STAGING': settings.STAGING,
-        'PRODUCTION': settings.PRODUCTION,
-        'BIGBAR': settings.BIGBUILD_BIGBAR,
-        'BRANCH': get_repo_branch(),
+        'BIGBUILD_BIGBAR': getattr(settings, 'BIGBUILD_BIGBAR', True),
+        'BIGBUILD_GIT_BRANCH': get_repo_branch(),
+        'BIGBUILD_BASE_URL': get_base_url()
     }
