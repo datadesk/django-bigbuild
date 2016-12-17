aedffa0e0107c7216dc887cbe4477a4ab632b53c
palewire <ben.welsh@gmail.com>
2016-12-17 14:38:53 -0800

    Cut the githubpages extension since we're going with RTD

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.githubpages']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -151,6 +151,3 @@ texinfo_documents = [
      author, 'django-bigbuild', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
