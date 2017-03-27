66b7efc15cb3b994fc44d5966a29c7f2d5453088
Ben Welsh <ben.welsh@gmail.com>
2017-03-27 12:01:07 -0700

    Removed dropcap template filter which should be upstream

--- bigbuild/templatetags/bigbuild_tags.py
+++ bigbuild/templatetags/bigbuild_tags.py
@@ -1,29 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import json
-from django import template
 from bigbuild.models import Page
-from django.utils.safestring import mark_safe
-from django.utils.html import conditional_escape
 register = template.Library()
-
-
-@register.filter(needs_autoescape=True)
-def dropcap(text, autoescape=True):
-    """
-    Wraps the first character in a <span> tag with a .dropcap class
-    that can be used to make it bigger than the surrounding text.
-    """
-    if not text:
-        return ''
-    first, other = text[0], text[1:]
-    if autoescape:
-        esc = conditional_escape
-    else:
-        def esc(x): return x
-    template = "<span class='dropcap'>{}</span>{}"
-    html = template.format(esc(first.upper()), esc(other))
-    return mark_safe(html)
 
 
 @register.filter
