53da488c2586032a5f53ed88eae1f785d1f0b79b
palewire <ben.welsh@gmail.com>
2017-04-02 15:01:28 -0700

    Upgraded django-bakery to 0.9.2

---
+++ bigbuild/management/commands/testserializer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from bigbuild.models import PageList
+from django.core.management.base import BaseCommand
+from django.core.serializers.python import Serializer
+
+
+class BigBuildSerializer(Serializer):
+    
+    def get_dump_object(self, obj):
+        data = OrderedDict([('model', force_text(obj._meta))])
+        if not self.use_natural_primary_keys or not hasattr(obj, 'natural_key'):
+            data["pk"] = force_text(obj._get_pk_val(), strings_only=True)
+        data['fields'] = self._current
+        return data
+
+
+class Command(BaseCommand):
+    help = 'Tests if page directories are valid'
+
+    def handle(self, *args, **options):
+        page_list = PageList()
+        page = page_list[0]
+
+        print page
