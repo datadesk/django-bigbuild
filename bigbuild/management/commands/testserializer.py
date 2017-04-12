d90c48c7f8592712e1774a15508cb40054b39481
palewire <ben.welsh@gmail.com>
2017-04-11 17:26:37 -0700

    flake8 fixes

--- bigbuild/management/commands/testserializer.py
+++ bigbuild/management/commands/testserializer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from bigbuild.models import PageList
+from collections import OrderedDict
+from django.utils.encoding import force_text
 from django.core.management.base import BaseCommand
 from django.core.serializers.python import Serializer
 
 
 class BigBuildSerializer(Serializer):
-    
+
     def get_dump_object(self, obj):
         data = OrderedDict([('model', force_text(obj._meta))])
         if not self.use_natural_primary_keys or not hasattr(obj, 'natural_key'):
@@ -21,5 +23,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         page_list = PageList()
         page = page_list[0]
-
         print page
