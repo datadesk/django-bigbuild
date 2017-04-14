afb50e0186930cdf7e07146b259b6036e93883e4
palewire <ben.welsh@gmail.com>
2017-04-14 12:12:08 -0700

    So long to all that

--- bigbuild/views.py
+++ bigbuild/views.py
@@ -2,15 +2,12 @@
 # -*- coding: utf-8 -*-
 import os
 import shutil
-import logging
 import bigbuild
-import multiprocessing
 from django.urls import reverse
 from django.http import Http404
 from django.conf import settings
 from django.views.static import serve
 from bigbuild import context_processors
-from multiprocessing.pool import ThreadPool
 from bigbuild.models import PageList, Page, ArchivedPage
 from bigbuild.management.commands.build import Command as Build
 from bakery.views import (
@@ -19,7 +16,6 @@ from bakery.views import (
     Buildable404View,
     BuildableRedirectView
 )
-logger = logging.getLogger(__name__)
 
 
 class BigBuildMixin(object):
@@ -178,9 +174,7 @@ class PageDetailView(BuildableDetailView, BigBuildMixin):
             target = obj.build_directory_path
             os.path.exists(target) and shutil.rmtree(target)
             if settings.BAKERY_GZIP:
-                cmd = Build()
-                cmd.no_pooling = True
-                cmd.copytree_and_gzip(
+                Build().copytree_and_gzip(
                     obj.archive_static_directory_path,
                     target
                 )
@@ -188,11 +182,7 @@ class PageDetailView(BuildableDetailView, BigBuildMixin):
                 shutil.copytree(obj.archive_static_directory_path, target)
 
     def build_queryset(self):
-        page_list = PageList()
-        cpu_count = multiprocessing.cpu_count()
-        logger.debug("Pooling build on {} CPUs".format(cpu_count))
-        pool = ThreadPool(processes=2)
-        pool.map(self.build_object, page_list)
+        [self.build_object(o) for o in PageList()]
 
 
 class PageArchiveView(PageDetailView):
