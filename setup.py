3268b5f6de0fa9abc622cad35f31402c31e278f7
palewire <ben.welsh@gmail.com>
2018-01-30 13:13:01 -0800

    v0.9.5

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ class TestCommand(Command):
 
 setup(
     name='django-bigbuild',
-    version='0.9.4',
+    version='0.9.5',
     description="The open-source engine that powers bigbuilder, the Los Angeles Times Data Desk's \
 system for publishing standalone pages",
     author='The Los Angeles Times Data Desk',
